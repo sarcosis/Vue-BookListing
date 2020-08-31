987735091aa97d2e2d63f715f7a7b057ff6e1da4
sarcosis <70417608+sarcosis@users.noreply.github.com>
2020-08-30 20:07:08 -0400

    Fixed so everything displays: Modlue 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-          <li v-for='book in books'>{{book.title}}:{{book.author}}</li>
+          <li v-for='book in books' :key='book.title'>{{book.title}} : {{book.author}}</li>
         </ul>
     </div>
 </template>
@@ -11,25 +11,15 @@
     export default {
         name: 'BookList',
         data() {
-            return
-            {
-              title: 'All Books'
+            return {
+              title: "All Books",
               books: [
-                 {
-                   title: 'Self-Reliance',
-                   author: 'Ralph Waldo Emerson',
-                 },
-                 {
-                   title: 'American Gods',
-                   author: 'Neil Gaiman',
-                 },
-                 {
-                   title: 'Amusing Ourselves to Death',
-                   authlor: 'Neil Postman',
-                },
-              ]
-            }
-        },
+                 { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+                 { title: 'American Gods', author: 'Neil Gaiman' },
+                 { title: 'Amusing Ourselves to Death', author: 'Neil Postman' }
+              ],
+            };
+        }
     }
 </script>
 
