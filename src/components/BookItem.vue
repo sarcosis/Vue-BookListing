2ee9261cc11e1044d3106c5c6417f473d9d5b53d
Victor Lam <victorlam03@gmail.com>
2020-08-30 21:17:35 -0400

    Adjusted Props

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -9,12 +9,7 @@
 <script>
     export default {
         name: 'BookItem',
-        props: {
-            book: [{
-                type: String,
-                default: '',
-            }],
-        },
+        props: ['book'],
     }
 </script>
 
